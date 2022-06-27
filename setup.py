f8cddbff4cc91dc210d579df76cd7dd66146f571
Tim <timischaf12@gmail.com>
2022-06-27 10:28:57 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='Playground',
+setup(name='playground',
       version='1.0',
       description='Google Active Learning Playground',
       author='Yilei Yang',
