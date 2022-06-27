a9fa5a280ae40a1f856911c0c534d493c2d7c9d0
Tim <timischaf12@gmail.com>
2022-06-27 10:42:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='playground',
       version='1.0',
       description='Google Active Learning Playground',
+      long_description=open("README.md", encoding="UTF-8").read(),
+      long_description_content_type="text/markdown",
       author='Yilei Yang',
       url='https://github.com/google/active-learning',
       packages=['playground','playground.utils','playground.sampling_methods'],
