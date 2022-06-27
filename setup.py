0f04102ea0a620d990919d47324605198555db3c
Tim <timischaf12@gmail.com>
2022-06-27 09:45:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='Playground',
       url='https://github.com/google/active-learning',
       packages=['playground','playground.utils','playground.sampling_methods'],
       package_dir={'playground':"active-learning",
-                   'playground.utils:"active-learning/utils",
+                   'playground.utils':"active-learning/utils",
                    'playground.sampling_methods':"active-learning/sampling_methods"}
      )
