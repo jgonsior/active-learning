57fdae631a89791fbe8cc9c77ccb17d6f78a9dfa
Julius Gonsior <julius@gaenseri.ch>
2023-01-04 16:15:37 +0100

    fixed hierarchical clustering

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="playground",
-    version="1.0",
+    version="1.0.1",
     description="Google Active Learning Playground",
     long_description=open("README.md", encoding="UTF-8").read(),
     long_description_content_type="text/markdown",
